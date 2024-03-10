9647ad47f864fc182d660962b3908096e89c84cd
juant21 <juan.esteban.tobias@gmail.com>
2024-03-10 13:42:06 -0500

    se esta agregando la estructura de la pagina

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>BerniShop</h1>
+      <p>Venta de una gran variedad de productos para el clima de tu preferencia.</p>
+    </div>
+  );
+}
+
+export default App;
