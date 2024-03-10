81d9d1294c7c6e88e9e4704b8d7e1fdda96f6cb8
juant21 <juan.esteban.tobias@gmail.com>
2024-03-10 15:37:01 -0500

    se aplico el uso de context

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Header from './componentes/header';
 import Footer from './componentes/footer';
 import './App.css';
 
+import {ShoppingCartProvider } from './context/carShoppingContext';
+
 function App() {
   return (
+    <ShoppingCartProvider>
     <div className="App">
       <Header />
       {/* Resto del contenido de la aplicación */}
       <Footer />
     </div>
+    </ShoppingCartProvider>
   );
 }
 
