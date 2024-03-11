5d3e8ed6e72a3eeed1c524df234d05fdae2b4960
juant21 <juan.esteban.tobias@gmail.com>
2024-03-10 20:48:38 -0500

    se corrigio un error en item

--- src/componentes/item.js
+++ src/componentes/item.js
@@ -1,11 +1,11 @@
 const Item = ({ producto }) => {
-  const { nombre, precio, imgUrl, } = producto;
+  const { name, price, imgUrl, } = producto;
 
   return (
     <div className="item">
-      <img src={imgUrl} alt={nombre} />
-      <h3>{nombre}</h3>
-      <p>Precio: ${precio}</p>
+      <img src={imgUrl} alt={name} />
+      <h3>{name}</h3>
+      <p>Precio: ${price}</p>
       <button>Agregar al carrito</button>
     </div>
   );
