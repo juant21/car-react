3fa12c18e891b4385f67a0538e590d00fdc27d68
juant21 <juan.esteban.tobias@gmail.com>
2024-03-10 23:40:21 -0500

    se agrego la Api de clima

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Footer from './componentes/footer';
 import productos from './componentes/productos';
 import ItemList from './componentes/itemList';
 import Carrito from "./componentes/carrito";
-
+import Clima from "./nuevoComponentes/Clima";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Carrito/>
       <h1>Productos:</h1>
       <ItemList listaProductos={productos} />
-      <Carrito/>
       <Footer />
+      
+      <Clima />
     </div>
   );
 }
